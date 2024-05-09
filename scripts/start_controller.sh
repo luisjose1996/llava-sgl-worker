56d63f5542d53a73c6b47c75ef721e53793bf781
luisjose1996 <luisnavedag10@gmail.com>
2024-05-08 21:26:38 -0500

    fix

--- scripts/start_controller.sh
+++ scripts/start_controller.sh
@@ -3,6 +3,6 @@ VENV_PATH=$(cat /workspace/LLaVA/venv_path)
 source ${VENV_PATH}/bin/activate
 cd /workspace/LLaVA
 nohup python3 -m llava.serve.controller \
-  --host ${LLAVA_HOST} \
+  --host 127.0.0.1 \
   --port ${LLAVA_CONTROLLER_PORT} > /workspace/logs/controller.log 2>&1 &
 deactivate
