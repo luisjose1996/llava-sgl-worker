8be9e0c527d58a1266abc81e196b987762b24eae
luisjose1996 <luisnavedag10@gmail.com>
2024-05-09 06:27:53 -0500

    wait for controller

--- scripts/start_sglang_worker.sh
+++ scripts/start_sglang_worker.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 VENV_PATH=$(cat /workspace/LLaVA/venv_path)
+
+# The endpoint to check
+URL="http://localhost:$SGL_ENDPOINT_PORT/get_model_info"
+
+# Wait for the HTTP status to be 200 OK
+HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+while [ $HTTP_STATUS -ne 200 ]; do
+  echo "Waiting for endpoint to be up..."
+  sleep 1 # Wait for 1 second before trying again
+  HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+done
+
+echo "Endpoint is up - Running script..."
 source ${VENV_PATH}/bin/activate
 cd /workspace/LLaVA
 
@@ -8,5 +21,5 @@ nohup python -m llava.serve.sglang_worker \
   --controller http://localhost:${LLAVA_CONTROLLER_PORT} \
   --port ${LLAVA_MODEL_SGLANG_WORKER_PORT} \
   --worker http://localhost:${LLAVA_MODEL_SGLANG_WORKER_PORT} \
-  --sgl-endpoint http://localhost:${SQL_ENDPOINT_PORT} > /workspace/logs/sgl-worker.log 2>&1 &
+  --sgl-endpoint http://localhost:${SGL_ENDPOINT_PORT} > /workspace/logs/sgl-worker.log 2>&1 &
 deactivate
